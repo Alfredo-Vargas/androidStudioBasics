0601194e5355f3294e9e94ccf10f8df4fd8cd3ef
pacha <pacha@archlen.localdomain>
2022-02-28 12:09:50 +0100

    dropmenu implemented

--- mc_lab0/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ mc_lab0/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.widget.Toast
@@ -20,27 +19,25 @@ class MainActivity : AppCompatActivity() {
         val mlayout: ConstraintLayout = findViewById(R.id.main_layout)
         val radioGroup: RadioGroup = findViewById(R.id.radio_grp)
 
+        // Buttons Listeners
         buttonred.setOnClickListener{
             Toast.makeText(this, "Button Red Clicked", Toast.LENGTH_SHORT).show()
             mlayout.setBackgroundColor(Color.RED)
         }
-
         buttonblue.setOnClickListener{
             Toast.makeText(this, "Button Blue Clicked", Toast.LENGTH_SHORT).show()
             mlayout.setBackgroundColor(Color.BLUE)
         }
-        radioGroup.setOnCheckedChangeListener { radioGroup, i ->
-            var rb :RadioButton = findViewById<RadioButton>(i)
-            if(rb!=null) {
-                if (radioGroup.checkedRadioButtonId.toString() == "radioBtnBlue")
-                    mlayout.setBackgroundColor(Color.BLUE)
-                else {
-                    mlayout.setBackgroundColor(Color.RED)
-                }
+
+        // RadioGroupListener
+        radioGroup.setOnCheckedChangeListener { radioGroup, id ->
+            val rbtnString = resources.getResourceEntryName(id)
+            if (rbtnString == "radioBtnBlue"){
+                mlayout.setBackgroundColor(Color.BLUE)
+            }
+            else {
+                mlayout.setBackgroundColor(Color.RED)
             }
         }
-
-        val radioblue: RadioButton = findViewById(R.id.radioBtnBlue)
-        val radiored: RadioButton = findViewById(R.id.radioBtnRed)
     }
 }
