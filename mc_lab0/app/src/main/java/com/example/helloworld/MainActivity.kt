1d5a94c9ebbdf5f2121c6819a9e5dd7fd120c884
Alfredo Vargas-Paredes <alfredo.vargas-paredes@outlook.com>
2022-03-01 11:32:58 +0100

    2022-03-01 11:3258

--- mc_lab0/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ mc_lab0/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,25 +3,24 @@ package com.example.helloworld
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get reference to buttons, radio buttons and layout
+        // Change BackgroundColor using buttons
         val buttonblue: Button = findViewById(R.id.btn_blue)
         val buttonred: Button = findViewById(R.id.btn_red)
         val mlayout: ConstraintLayout = findViewById(R.id.main_layout)
         val radioGroup: RadioGroup = findViewById(R.id.radio_grp)
 
-        // Buttons Listeners
         buttonred.setOnClickListener{
-            //Toast.makeText(this, "Button Red Clicked", Toast.LENGTH_SHORT).show()
             mlayout.setBackgroundColor(Color.RED)
         }
         buttonblue.setOnClickListener{
@@ -42,11 +41,34 @@ class MainActivity : AppCompatActivity() {
         val spinnerlanguages: Spinner = findViewById(R.id.spLanguages)
         var selectedlang: TextView = findViewById(R.id.selectedLanguage)
 
-        // Object is an anonymous class
-        spinnerlanguages.onItemSelectedListener()
+        // Dropdowm Menu and TextView Display selected Item
+        spinnerlanguages.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {  // object is anonymous class!
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedlang.setText(spinnerlanguages.getSelectedItem().toString())
+            }
 
-
+            // No need because we have always something selected (we leave this blank)
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
         }
+
+        // TextWatcher
+        var mutabletext: TextView = findViewById(R.id.textMutable)
+        var edittextfield: EditText = findViewById(R.id.editText)
+
+        edittextfield.addTextChangedListener(object : TextWatcher {
+            // No need to do action before text changes
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                mutabletext.setText(edittextfield.editableText)
+            }
+
+            // No need to do actions after text changes
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
 }
 
