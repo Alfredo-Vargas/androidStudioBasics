c097f86660a5bc77ba882c359fbb7a51a0ccce13
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-03-24 23:28:41 +0100

    2022-03-24 23:2841

--- final_project/app/src/main/java/com/example/memoryschest/MainActivity.kt
+++ final_project/app/src/main/java/com/example/memoryschest/MainActivity.kt
@@ -7,7 +7,17 @@ package com.example.memoryschest
 // (1) Champagne: #F4E2C8
 // (2) Liver Chestnut: #99765F
 // (3) Blue Yonder: #5D74BC
-
+// (4) Gris: #5a5a5a
+// Second theme
+// #191919
+// #2D4263
+// #C84B31
+// #ECDBBA
+// Third theme
+// #B5EAEA
+// #EDF6E5
+// #FFBCBC
+// #F38BA0
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
